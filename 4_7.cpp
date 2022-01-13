b8b56a9a554417ac2e3d20b7c0689b337da4136d
Kiryl <katana96@mail.ru>
2022-01-13 13:48:36 +0300

    Exercise4_7,8

---
+++ 4_7.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int x, y;
+
+    cin >> x;
+    cin >> y;
+
+    int i=1;
+    int power=1;
+
+    power*=x;
+    ++i;
+
+    if (i<=y)
+    
+       cout << endl << i << "<=" << y;
+    
+    //Возведение x в степень у. 4_8:
+    while (i<=y)
+    {
+        power=power*x;
+        i=i+1;
+    }
+
+    cout << endl << power;
+    
+    
+}
