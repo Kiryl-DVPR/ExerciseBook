16761cd803c9068f4d3a7a55e36b31a35cf9b22f
Kiryl <katana96@mail.ru>
2022-01-13 22:07:35 +0300

    Exercise4_13Finish

--- 4_13.cpp
+++ 4_13.cpp
@@ -4,22 +4,33 @@ using namespace std;
 
 int main()
 {
-    int x; //Пройденное растояние в милях
-    int y; //Количество бензина в галлонах
-    double z; //Количество миль/галлон
+    int Distance; //Пройденное растояние в милях
+    int Refueling; //Количество бензина в галлонах(дозапрвка)
+    double FuelConsumption; //Количество миль/галлон
+    double SumFuelConsumption=0; //Суммарное значени расхода
+    int SumRefueling=0; //Счётчик для количества дозаправок
 
-    while (x!=0)
+    cout << endl << "Enter distance of miles (or 0 to quit): ";
+    cin >> Distance;
+
+    while (Distance!=0)
     {
-        cout << endl << "Enter number of miles: ";
-        cin >> x;
-
+        
         cout  << "Enter the number of gallons of gasoline : ";
-        cin >> y;
+        cin >> Refueling;
 
-        z=static_cast<double>(x)/static_cast<double>(y);
+        FuelConsumption=static_cast<double>(Distance)/Refueling;
 
-        cout  << "Gasoline consumption miles/gallon: " << setprecision(4) << fixed <<z ;
+        cout  << "Gasoline consumption miles/gallon: " << setprecision(4) << fixed << FuelConsumption <<endl ;
 
+        SumRefueling=SumRefueling+1;
+        SumFuelConsumption=(SumFuelConsumption + FuelConsumption)/SumRefueling;
+
+        cout  << "Average fuel consumption miles/gallon: " << setprecision(4) << fixed << SumFuelConsumption <<endl ;
+
+        cout << endl << "Enter distance of miles (or 0 to quit): ";
+        cin >> Distance;
 
     }
+
 }
