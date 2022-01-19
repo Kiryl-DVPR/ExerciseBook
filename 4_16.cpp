2d5a6f39e14ad0e43d8bedc917670921c71a0ec4
Kiryl <katana96@mail.ru>
2022-01-19 11:19:39 +0300

    Exercise 4_16 Finish

--- 4_16.cpp
+++ 4_16.cpp
@@ -8,30 +8,31 @@ int main()
     float HourlyRate; //Почасовая ставка
     float Salary;
 
-    cout << "Enter the number of working hours (or -1 for quit): "; //Введите число рабочих часов
-    cin >> WorkHours;
+        cout << "Enter the number of working hours (or -1 for quit): "; //Введите число рабочих часов
+        cin >> WorkHours;
 
     while ( WorkHours!=-1 )
     {
         cout << "Enter the hourly of rate: "; //Введите число рабочих часов
         cin >> HourlyRate;
+
         if ( WorkHours<=40 )
-        {
-            Salary=HourlyRate*WorkHours;
+            {
+            Salary=HourlyRate*WorkHours; //Вычисляем ЗП при условии Число рабочих часов <= 40
 
             cout << "Salary: " << Salary <<endl;
-        }
+            }
         else 
             {
             
-            Salary=( WorkHours%40 )*( HourlyRate*1.5 )+40*HourlyRate;
+            Salary=( WorkHours-40 )*( HourlyRate*1.5 )+40*HourlyRate; //Вычисляем ЗП при условии Число рабочих часов > 40
 
             cout << "Salary: " << Salary << endl;
 
             }
         
         cout << "Enter the number of working hours (or -1 for quit): "; //Введите число рабочих часов
-    cin >> WorkHours;
+        cin >> WorkHours;
     }
     
 }
