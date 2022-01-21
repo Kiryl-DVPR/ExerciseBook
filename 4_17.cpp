b16b261831673bf9f354c9068fce3fe785e454c2
Kiryl <katana96@mail.ru>
2022-01-21 14:09:51 +0300

    Exercise 4_17 Finish

--- 4_17.cpp
+++ 4_17.cpp
@@ -4,3 +4,29 @@ using namespace std;
 
 int main()
 {
+    int counter=1; //Счётчик для счёта до 10
+    int number=0; //Текущее введенное число
+    int argest=0; //Максимальное найденоое число
+
+    cout << "Enter 10 numbers: " << endl; 
+
+    while (counter<=10)
+    {
+        
+        cout << counter <<" Number: ";
+        cin >> number;
+        
+      
+        if (number >= argest)
+        {
+            argest = number;
+        }
+
+        counter=counter+1;
+        
+
+    }
+
+    cout << endl << "Maximum number: " << argest;
+    
+}
