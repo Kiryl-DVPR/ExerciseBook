4940d01295211fc6a4e1b9a93677c72eeaf9154e
Kiryl <katana96@mail.ru>
2022-01-24 15:53:20 +0300

    Exercise 4_19 Finish

--- 4_19.cpp
+++ 4_19.cpp
@@ -6,27 +6,35 @@ int main()
 {
     int counter=1; //Счётчик для счёта до 10
     int number=0; //Текущее введенное число
-    int argest=0; //Максимальное найденоое число
+    int firstArgest=0; //Первое максимальное найденоое число
+    int secondArgest=0; //Первое максимальное найденоое число
 
     cout << "Enter 10 numbers: " << endl; 
 
     while (counter<=10)
     {
-        
         cout << counter <<" Number: ";
         cin >> number;
-        
-      
-        if (number >= argest)
-        {
-            argest = number;
-        }
 
-        counter=counter+1;
+            if(secondArgest>=number) //Сравниваем secondArgest с введённым number
+                {
+                    secondArgest = number;
+                }
         
+            if (number >= firstArgest)
+                {
+                    firstArgest = number;
+                } 
+            else
+                {
+                    secondArgest = number; //Сохраняем number, который не прошёл условие number >= firstArgest 
+                }
+       
+        counter=counter+1;  
 
     }
 
-    cout << endl << "Maximum number: " << argest;
+    cout << endl << "1 Maximum number: " << firstArgest << endl;
+    cout << endl << "2 Maximum number: " << secondArgest;
     
 }
