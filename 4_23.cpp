be183ac86cb6561d884cb524548b0ce0fd7806f0
Kiryl <katana96@mail.ru>
2022-01-28 13:54:10 +0300

    Exercise 4_23

---
+++ 4_23.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+#include <iomanip>
+
+int main()
+{
+    int x=11;
+    int y=9;
+
+    if (x<y)
+    if (y>10)
+    cout << "*****" << endl;
+    else
+    cout << "#####" << endl;
+    cout << "$$$$$" << endl;
+
+    }
