d77bc01be350e87d09b9ce41e3f03197df030c95
Kiryl <katana96@mail.ru>
2022-01-30 13:29:20 +0300

    Exercise 4_25 Finish

--- 4_25.cpp
+++ 4_25.cpp
@@ -5,26 +5,43 @@ using namespace std;
 int main()
 {
     int lenthSquare; //Длина квадрата
-    int lenth1=1;//Счётчик1
-    int lenth2=1;//Счётчик1
-    int z;
+    int lenth1=1;//Счётчик для верхней стороны
+    int lenth2=1;//Счётчик для левой сороны
+    int lenth3=1;//Счётчик для нижней стороны
+    
 
     cout << "Enter lenth of saqure: ";
     cin >> lenthSquare;
 
-    while (lenthSquare>=lenth1)
-    {
-        cout << "*";
-        ++lenth1;
-    }
-    
-    while (lenthSquare>=lenth2)
-    {   
-        
-        z=lenthSquare-2;
+        while (lenthSquare>=lenth1) //Для вывода верхней стороны квадрата
+        {
+            cout << "*";
+            ++lenth1;
+        }
 
-        cout << "*\n";
-        ++lenth2;
-    }
+    cout << "\n";
+
+        while ((lenthSquare-1)>lenth2) // Для вывода левой боковой стороны квадрата
+        {   
+            cout << "*";
+
+            int lenth4=1;
+
+                while ((lenthSquare-1)>lenth4) // Для вывода правой боковой стороны
+                {   
+                    cout << " ";
+                    ++lenth4;
+                }
+
+            cout << "*\n";
+            
+            ++lenth2;
+        }
+
+        while (lenthSquare>=lenth3) //Для вывода нижней стороны квадрата
+        {   
+            cout << "*";
+            ++lenth3;
+        }
     
 }
