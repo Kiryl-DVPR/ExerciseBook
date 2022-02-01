940fa76234464b9ca0c155d7f4611d74532d62f9
Kiryl <katana96@mail.ru>
2022-02-01 09:12:47 +0300

    Exercise 4_27Finish

--- 4_27.cpp
+++ 4_27.cpp
@@ -6,37 +6,35 @@ int main()
 {
     int numberBinary;//Двоичное число
     int n=10;
-    int c=2;//Счётчик для цикла
-    int x;
-    int z=1;
-    int e=10;
-    int sum=0;
-    int y=1;
+    int c=2;//Счётчик для цикла, который определяет количество цифр в двоичном коде
+    int mod;//Остаток от деления двоичного кода
+    int z=1;//Счётчик для цикла перевода двоичного кода в десятичный
+    int sum=0;//Перевод из двоичного кода в десятичный
+    int y=1;//Элемент перевода в десятичный код, умножение на цифры двоичного кода начинается с конца двоичного кода и равно 1,2,4,8,16...
     
     cout << "Enter a binary number: ";
     cin >> numberBinary;
 
-    while (numberBinary/n>1)
+    while (numberBinary/n>1)//Для определения количества цифр в двоичном коде
     {
         n=n*10;
         ++c;
     }
 
-    cout << endl << c;
-
-    while (z<=c)
+    while (z<=c)//Цикл, который должен повториться "c" раз, т.е. количество цифр в двоичеом коде
     {
-        x=numberBinary%(n/e);
-        e=e*10;
+        mod=numberBinary%10;
+
+        numberBinary=(numberBinary-mod)/10;
+
+        sum=sum+mod*y;
 
         y=y*2;
         
-        sum=sum+x*y;
         ++z;
-        
     }
     
-    cout << endl << sum;
+    cout << "Convert to Decimal: " << sum;
     
-
+            
 }
