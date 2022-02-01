eaf59e12b686012d5b15ada22cdf4e6fc0f7558f
Kiryl <katana96@mail.ru>
2022-02-01 20:55:24 +0300

    Exercise 4_29 Finish

---
+++ 4_29.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+#include <iomanip>
+
+int main()
+{
+    int prod=2;
+
+    while(prod>=0)
+    {
+        prod=prod*2;
+        cout << prod << " ";
+
+    }
+}
