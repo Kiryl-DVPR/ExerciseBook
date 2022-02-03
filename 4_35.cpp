04a920864667e74c7a184ca187d2853648fc54ed
Kiryl <katana96@mail.ru>
2022-02-03 09:35:43 +0300

    Exercise 4_35 Finish

--- 4_35.cpp
+++ 4_35.cpp
@@ -4,20 +4,31 @@ using namespace std;
 
 int main()
 {
-    int number=1234;
+    int number;
     int firstLvCode;//Первая операция кодировки
     int a=1000;//Счётчик для цикла
-    int c;//Целая часть числа
+    int c1, a1;//Целая часть числа
+    int c2, a2;
+    int c3, a3;
+    int c4, a4;
 
-    cout << "Accepted data from the company: " << number << endl;
+    cout << "Accepted data from the company: ";
+    cin >> number;
+    
+    c1=(number/1000);//Находим цифру 1
+    a1=(c1+7)/10;
 
-    while(a>=1)
-    {
-        c=number/a;//Находим целую часть числа
-        firstLvCode=(c+7)/10;//Первая степень кодировки
-        number=number-(c*a);//Определяем число за вычетом целой части
-        a=a/10;
-        cout <<firstLvCode;
-    }
+    c4=(number%10);
+    a4=(c4+7)/10;
+
+    c2=((number-(c1*1000))/100);
+    a2=(c2+7)/10;
+
+    c3=((number-c4)/10)%10;
+    a3=(c3+7)/10;
+    
+
+    cout << "Encrypted message: "<< a4 <<a3 << a2 << a1;   
+    
     
 }
