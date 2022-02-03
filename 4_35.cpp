462ed57b76f114ff8b314660dddd9d21dd858734
Kiryl <katana96@mail.ru>
2022-02-03 10:16:26 +0300

    Exercise 4_35 First path  Finish

--- 4_35.cpp
+++ 4_35.cpp
@@ -5,30 +5,27 @@ using namespace std;
 int main()
 {
     int number;
-    int firstLvCode;//Первая операция кодировки
-    int a=1000;//Счётчик для цикла
-    int c1, a1;//Целая часть числа
-    int c2, a2;
-    int c3, a3;
-    int c4, a4;
+    int c1; /*Первая цифра*/ float a1;/*Зашифрованная первая цифра*/
+    int c2; /*Вторая цифра*/ float a2;/*Зашифрованная вторая цифра*/
+    int c3; /*Третья цифра*/ float a3;/*Зашифрованная третья цифра*/
+    int c4; /*Четвёртая цифра*/ float a4;/*Зашифрованная четвёртая цифра*/
 
     cout << "Accepted data from the company: ";
     cin >> number;
     
-    c1=(number/1000);//Находим цифру 1
-    a1=(c1+7)/10;
+    c1=number/1000;//Находим 1 цифру 
+    a1=(static_cast< float >(c1)+7)/10; //Шифруем 1 цифру
 
-    c4=(number%10);
-    a4=(c4+7)/10;
+    c4=number%10; //Находим 2 цифру
+    a4=(static_cast< float >(c4)+7)/10; //Шифруем 2 цифру
 
-    c2=((number-(c1*1000))/100);
-    a2=(c2+7)/10;
+    c2=((number-(c1*1000))/100); //Находим 3 цифру
+    a2=(static_cast< float >(c2)+7)/10; //Шифруем 3 цифру
 
-    c3=((number-c4)/10)%10;
-    a3=(c3+7)/10;
+    c3=((number-c4)/10)%10; //Находим 4 цифру
+    a3=(static_cast< float >(c3)+7)/10; //Шифруем 4 цифру
     
 
-    cout << "Encrypted message: "<< a4 <<a3 << a2 << a1;   
-    
+    cout << "Encrypted message: "<< a3 << " " <<a4 << " " << a1 << " "<< a2;   
     
 }
